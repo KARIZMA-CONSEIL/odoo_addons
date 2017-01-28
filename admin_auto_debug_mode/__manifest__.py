100b1b7d2a87b9386212c972d700357e70e322a5
Saad Thaifa <saad.thaifa@gmail.com>
2017-01-28 14:54:20 +0000

    [ADD] Auto Admin developer mode

---
+++ admin_auto_debug_mode/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+# Copyright (C) 2017 Karizma Conseil (<http://karizma.ma>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name' : 'Admin auto developer mode',
+    'version' : '10.0',
+    'author' : 'karizma.ma',
+    'website' : 'http://www.karizma.ma',
+    "category": "Tools",
+    'summary': 'Backups',
+    'description': """
+""",
+    'depends' : ['base'],
+    'data': [
+    ],
+    'installable': True
+}
---
+++ admin_auto_debug_mode/controllers/main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import http, _
+import odoo
+from odoo.http import route
+from odoo.http import request 
+from odoo.addons.web.controllers.main import Home, ensure_db
+
+class DebugMode(Home):
+
+    @http.route('/web/login', type='http', auth="none")
+    def web_login(self, redirect=None, **kw):
+        ensure_db()
+        request.params['login_success'] = False
+        if request.httprequest.method == 'GET' and redirect and request.session.uid:
+            return http.redirect_with_hash(redirect)
+
+        if not request.uid:
+            request.uid = odoo.SUPERUSER_ID
+
+        values = request.params.copy()
+        try:
+            values['databases'] = http.db_list()
+        except odoo.exceptions.AccessDenied:
+            values['databases'] = None
+        if request.httprequest.method == 'POST':
+            old_uid = request.uid
+            uid = request.session.authenticate(request.session.db, request.params['login'], request.params['password'])
+            if uid is not False:
+                request.params['login_success'] = True
+                if not redirect:
+                    if request.uid == 1:
+                        redirect = '/web?debug=1'
+                    else:
+                        redirect = '/web'
+                return http.redirect_with_hash(redirect)
+            request.uid = old_uid
+            values['error'] = _("Wrong login/password")
+        return request.render('web.login', values)
